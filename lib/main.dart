0db2870fc98ebeada4cd9e36be3f80726e67b27c
Allan Teles <sistemas@valadares.mg.gov.br>
2025-06-06 14:11:17 -0300

    trocando titulo

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Menu With NavigationRail',
+      title: 'Menu',
       // This is the theme of your application.
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
